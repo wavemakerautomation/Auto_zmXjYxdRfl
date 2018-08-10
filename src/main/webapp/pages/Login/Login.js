df5304fd43b3a2b23030b2f81e366311fcb82a5e
auto11 user11 <automate11@wavemaker.com>
2018-08-10 21:55:28 +0000

    Initial Push

---
+++ src/main/webapp/pages/Login/Login.js
@@ -0,0 +1,17 @@
+/*
+ * Use Injector.get for Dependency Injection
+ * eg: var $timeout = Injector.get('$timeout');
+ */
+
+/* perform any action on widgets/variables within this block */
+Page.onReady = function () {
+    /*
+     * variables can be accessed through 'Page.Variables' property here
+     * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
+     * Page.Variables.loggedInUser.getData()
+     *
+     * widgets can be accessed through 'Page.Widgets' property here
+     * e.g. to get value of text widget named 'username' use following script
+     * 'Page.Widgets.username.datavalue'
+     */
+};
